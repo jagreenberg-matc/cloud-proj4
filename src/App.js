75822a52a19a0039f71a89e9b073fa185091156f
Jack Greenberg <jagreenberg@madisoncollege.edu>
2025-05-14 16:33:40 -0500

    updated App.js to render my full name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jack Greenberg
         </p>
         <a
           className="App-link"
